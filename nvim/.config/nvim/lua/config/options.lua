dc1ec971fcc1d8af9b145d176365bd446b4710b8
Sanket Nayak <sanket1598@ugmail.com>
2026-06-03 17:30:05 -0700

    nvim: remap j/k/0/$/^ to visual line movement when wrap is on

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,11 @@
 
 vim.opt.wrap = true
 vim.opt.linebreak = true    -- wrap at word boundaries, not mid-word
+
+-- Make j/k and Home/End move by visual line when wrap is on
+vim.keymap.set({ "n", "x" }, "j",   "v:count == 0 ? 'gj' : 'j'",  { expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "k",   "v:count == 0 ? 'gk' : 'k'",  { expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "$",   "g$",  { silent = true })
+vim.keymap.set({ "n", "x" }, "0",   "g0",  { silent = true })
+vim.keymap.set({ "n", "x" }, "^",   "g^",  { silent = true })
+vim.keymap.set("i",          "<End>", "<C-o>g$", { silent = true })
