167b33dd858709d94a1261f558bd9409fa2c49f7
Sanket Nayak <sanket1598@ugmail.com>
2026-06-03 17:31:21 -0700

    nvim: revert /usr/bin/zsh visual remaps (broke A); keep only j/k visual movement

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,10 +5,6 @@
 vim.opt.wrap = true
 vim.opt.linebreak = true    -- wrap at word boundaries, not mid-word
 
--- Make j/k and Home/End move by visual line when wrap is on
-vim.keymap.set({ "n", "x" }, "j",   "v:count == 0 ? 'gj' : 'j'",  { expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "k",   "v:count == 0 ? 'gk' : 'k'",  { expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "$",   "g$",  { silent = true })
-vim.keymap.set({ "n", "x" }, "0",   "g0",  { silent = true })
-vim.keymap.set({ "n", "x" }, "^",   "g^",  { silent = true })
-vim.keymap.set("i",          "<End>", "<C-o>g$", { silent = true })
+-- Make j/k move by visual line when wrap is on (only without a count)
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
